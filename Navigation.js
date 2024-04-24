544ede460c73bad9f584fc540ccb1c004a1bd2b1
milan-brouns <milanbrouns@hotmail.com>
2024-04-24 22:48:15 +0200

    navigation foundations

---
+++ Navigation.js
@@ -0,0 +1,12 @@
+import {NavigationContainer} from '@react-navigation/native'
+import Home from './screens/Home'
+
+function Navigation() {
+    return (
+        <NavigationContainer>
+            <Home/>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation;
---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+  } from 'react-native';
+
+function Home(){
+    return(
+        <SafeAreaView>
+            <Text>Sup, jonge!</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
