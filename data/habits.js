eb968469f994eaa8592ac79a9bb8c4f7054f3146
milan-brouns <milanbrouns@hotmail.com>
2024-04-27 22:17:32 +0200

    adding first data

---
+++ data/habits.js
@@ -0,0 +1,11 @@
+const habits ={
+    {
+        name:"Brush theeth"
+        startDate:"20/04/2024"
+        frequency:"daily-twice"
+        tracking:{
+            date:"20/04/2024"
+            status1:
+        }
+    }
+}
